6b7f9a049dedb9f8110f781ccf02da6d962d9e0c
saanidhyagoyal <103800120+saanidhyagoyal@users.noreply.github.com>
2023-12-20 12:38:12 +0530

    Time: 3 ms (86.46%), Space: 21.1 MB (23.47%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,51 +1,3 @@
-// class Solution {
-// public:
-//     string reverseWords(string s) {
-//         //char temp;
-//         int l = s.length();
-//         int start=0,a;
-//         int end = l-1;
-//         string x="",y="",temp="";
-//         while(start<end){
-//             if(isspace(s[start])){
-//                 while(isspace(s[end])){
-//                     x=s.substr(a,start);
-//                     y=s.substr(end,l);
-//                     swap(y,x);
-//                     s=s.substr(start,end);
-//                     s=x+s+y;
-//                     end--;
-//                     l=end;
-//                 }
-//                 start++;
-//                 a=start;
-
-//             }
-//             else if(isspace(s[end])){
-//                 while(isspace(s[start])){
-//                     x=s.substr(a,start);
-//                     y=s.substr(end,l);
-//                     swap(y,x);
-//                     s=s.substr(start,end);
-//                     s=x+s+y;
-//                 }
-//                 end--;
-//             }
-//             /*else {
-//                 x+=s.substr(end+1,l);
-//                 x+= " ";
-//                 x+=s.substr(0,start);
-//                 start++;end--;
-//                 s=s.substr(start,end);
-
-//             }*/
-//         }
-//         return x;
-//     } 
-    
-// };
-
-
 class Solution {
 public:
     string reverseWords(string s) {
@@ -74,7 +26,6 @@ public:
         if (!temp.empty()) {
             ans = temp + " " + ans;
         }
-
         if(!ans.empty()){
             ans=ans.substr(0,ans.length()-1);
         }
